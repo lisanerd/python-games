a44f189bbf304b2e2425fad264519dee89503a00
lisanerd <lisa.rm.nyc@gmail.com>
2021-09-29 19:13:21 -0400

    Started a new program called "Rainbow". Edited it a bit from original program in first commit.

---
+++ Rainbow.py
@@ -0,0 +1,54 @@
+
+
+
+
+
+import time, sys
+
+try:
+    import bext
+except ImportError:
+    print('This program requires the bext module, which you')
+    print('can install by following the instructions at')
+    print('https://pypi.org/project/Bext/')
+    sys.exit()
+
+print('Rainbow, by Al Sweigart al@inventwithpython.com')
+print('Press Ctrl-C to stop.')
+time.sleep(3)
+
+indent = 1 # how many spaces to indent
+indentIncreasing = True # whether the indentation is increasing or not
+
+try:
+    while True: # main program loop
+        print(' ' * indent, end='')
+        bext.fg('random')
+        print('lisa', end='')
+        bext.fg('green')
+        print('lisa', end='')
+        bext.fg('random')
+        print('lisa', end='')
+        bext.fg('blue')
+        print('lisa', end='')
+        bext.fg('random')
+        print('lisa', end='')
+        bext.fg('red')
+        print('lisa')
+
+        if indentIncreasing:
+            # increase the number of spaces
+            indent = indent + 1
+            if indent == 60:
+                # change direction
+                indentIncreasing = False
+        else:
+            # decrease the number of spaces
+            indent = indent - 1
+            if indent == 0:
+                # change direction:
+                indentIncreasing = True
+        
+        time.sleep(0.02) # add slight pause
+except KeyboardInterrupt:
+    sys.exit() # when Ctrl-C is pressed, end the program
